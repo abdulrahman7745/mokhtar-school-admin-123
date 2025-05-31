8172d3cb60c79afbde7d72c4de723115881040c2
abdulrahman7745 <abdulrahmanabdullah774@gmail.com>
2025-05-31 16:41:39 +0300

    Create App.jsx

---
+++ src/srs/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ fontFamily: 'Arial', textAlign: 'center', marginTop: '50px' }}>
+      <h1>🎓 مدارس المختار الأهلية</h1>
+      <p>تطبيق الإدارة وأولياء الأمور</p>
+    </div>
+  )
+}
+
+export default App
